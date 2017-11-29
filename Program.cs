283990cec70748267bb0dbeae5940ba3f006713c
bin senpai <theislamicgamer101@gmail.com>
2017-11-29 15:28:56 -0500

    ccc

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordGatewayGettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
