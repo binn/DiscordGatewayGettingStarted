4706d8ce4b8f348a732750ba633ddbcc0dc5a373
bin senpai <theislamicgamer101@gmail.com>
2017-11-29 15:50:15 -0500

    g

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
 ﻿using System;
 
+/*
+    Hey there ! Welcome to this little simple meme I put together, 
+    For now, most of this code is uncommented and soon will be commented
+
+    To build and run this use
+
+    `dotnet restore`
+    `dotnet run`
+
+    or if you want a built version:
+    `dotnet build` (you can also output to a different folder)
+    `dotnet {path_to_built_file_here}`
+    
+     
+ */
+
 namespace DiscordGatewayGettingStarted
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
